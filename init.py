6d0435a96cd7a97fa597f54c22f09d4619fea893
mrust <m.rustam1305@gmail.com>
2023-05-23 14:04:26 +0500

    init

---
+++ init.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+
+for i in range(27):
+    cur_dir = f'tasks/task{i + 1}'
+    try:
+        os.mkdir(cur_dir)
+    except FileExistsError:
+        pass
+    levels = ['homework', 'base', 'middle', 'hard', 'pominki']
+    for level in levels:
+        try:
+            os.mkdir(f'{cur_dir}/{level}')
+        except FileExistsError:
+            pass
+        
+        if len(os.listdir(f'{cur_dir}/{level}')) > 0:
+            os.remove(f'{cur_dir}/{level}/temp.txt')
+        else: 
+            with open(f'{cur_dir}/{level}/temp.txt', mode='w') as f:
+                f.write('File to add dir to git')
