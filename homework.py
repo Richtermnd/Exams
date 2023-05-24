28df6823ed509dffb3815fa25d61b01874635d53
mrust <m.rustam1305@gmail.com>
2023-05-24 22:59:31 +0500

    task1 homework

--- homework.py
+++ homework.py
@@ -5,12 +5,12 @@ import os
 _, task_n, n = sys.argv
 
 if os.path.exists(f'tasks/task{task_n}/homework/template.py'):
-    with open(f'tasks/task{task_n}/homework/template.py') as f:
+    with open(f'tasks/task{task_n}/homework/template.py', encoding='utf8') as f:
         data = f.read()
 else:
     data = ''
 
 for i in range(int(n)):
     if not os.path.exists(f'tasks/task{task_n}/homework/n{i + 1}.py'):
-        with open(f'tasks/task{task_n}/homework/n{i + 1}.py', mode='w') as f:
+        with open(f'tasks/task{task_n}/homework/n{i + 1}.py', mode='w', encoding='utf8') as f:
             f.write(data)
