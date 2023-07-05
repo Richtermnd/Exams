0ccbfcc0c31b95046cb96bdff65f92ad338253ec
mrust <m.rustam1305@gmail.com>
2023-07-06 01:42:41 +0500

    Task27 Reset.

--- tasks/task27/homework/template.py
+++ tasks/task27/homework/template.py
@@ -1,19 +1,12 @@
 import os
 
 
-os.chdir('tasks/task27/homework/n31/')
+os.chdir('tasks/task27/homework/n_/')
 
 
 def solution(var):
     f = open(f'27{var}.txt', mode='r')
-    n, m = [int(x) for x in f.readline().split()]
-
-    a = [int(f.readline()) for _ in range(n)]  # массив размером n, в котором хранится кол-во едениц для каждого пункта
-    
-    res = s = sum(a[:2 * m + 1])
-    for i in range(2 * m + 1, n - m):
-        pass
-
+    res = ...
     print(res)
 
 
